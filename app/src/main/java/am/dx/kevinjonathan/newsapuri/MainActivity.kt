66fbfd8bbcbc58c6601c97f86f3ebbe7e144c61c
Kevin Jonathan <kevinjonathan3010@gmail.com>
2024-01-12 13:51:17 +0700

    Add splash screen

--- app/src/main/java/am/dx/kevinjonathan/newsapuri/MainActivity.kt
+++ app/src/main/java/am/dx/kevinjonathan/newsapuri/MainActivity.kt
@@ -3,11 +3,13 @@ package am.dx.kevinjonathan.newsapuri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import am.dx.kevinjonathan.newsapuri.ui.theme.NewsApuriTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsApuriTheme {
 
